e03e78210cafe9ae7e93ce0e12ca7af493f650d6
tarthur <terzi.arthur@gmail.com>
2019-08-27 10:15:28 +0300

    create gh-pages

--- README.md
+++
@@ -1,5 +0,0 @@
-# `Tic-tac-toe upgrade`
-
-Reactjs.org tutorial [https://reactjs.org/tutorial/tutorial.html](https://reactjs.org/tutorial/tutorial.html)
-
-Reactjs.org tutorial codepen.io code [https://codepen.io/gaearon/pen/gWWZgR?editors=0010](https://codepen.io/gaearon/pen/gWWZgR?editors=0010)
---
+++ precache-manifest.7306fe46e3fa6db220f3c04878158a0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de9f82981c322f94da847c0ff1429ee9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71e7da5104ec4b0a7248",
+    "url": "/static/css/main.ca066122.chunk.css"
+  },
+  {
+    "revision": "70bec262e6977e9078c5",
+    "url": "/static/js/2.30d259f8.chunk.js"
+  },
+  {
+    "revision": "71e7da5104ec4b0a7248",
+    "url": "/static/js/main.94ceacd4.chunk.js"
+  },
+  {
+    "revision": "879413fbb1a3fcf2057a",
+    "url": "/static/js/runtime~main.4b478a9b.js"
+  }
+]);
